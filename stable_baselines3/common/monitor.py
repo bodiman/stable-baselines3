d8b0b2e9690fd7008ab2cba264ae99985c66c02d
bodiman <bodiszabo@gmail.com>
2023-01-10 12:28:04 -0800

    errorhandling

--- stable_baselines3/common/monitor.py
+++ stable_baselines3/common/monitor.py
@@ -89,30 +89,31 @@ class Monitor(gym.Wrapper):
         :param action: the action
         :return: observation, reward, done, information
         """
+        try: 
+            if self.needs_reset:
+                raise RuntimeError("Tried to step environment that needs reset")
+                
+            observation, reward, done, info = self.env.step(action)
+            self.rewards.append(reward)
+            if done:
+                self.needs_reset = True
+                ep_rew = sum(self.rewards)
+                ep_len = len(self.rewards)
+                ep_info = {"r": round(ep_rew, 6), "l": ep_len, "t": round(time.time() - self.t_start, 6)}
+                for key in self.info_keywords:
+                    ep_info[key] = info[key]
+                self.episode_returns.append(ep_rew)
+                self.episode_lengths.append(ep_len)
+                self.episode_times.append(time.time() - self.t_start)
+                ep_info.update(self.current_reset_info)
+                if self.results_writer:
+                    self.results_writer.write_row(ep_info)
+                info["episode"] = ep_info
+            self.total_steps += 1
+            return observation, reward, done, info
         
-        raise("your code is running")
-        if self.needs_reset:
-            raise RuntimeError("Tried to step environment that needs reset")
-        x = self.env.step(action)
-        print(x)
-        observation, reward, done, info = x
-        self.rewards.append(reward)
-        if done:
-            self.needs_reset = True
-            ep_rew = sum(self.rewards)
-            ep_len = len(self.rewards)
-            ep_info = {"r": round(ep_rew, 6), "l": ep_len, "t": round(time.time() - self.t_start, 6)}
-            for key in self.info_keywords:
-                ep_info[key] = info[key]
-            self.episode_returns.append(ep_rew)
-            self.episode_lengths.append(ep_len)
-            self.episode_times.append(time.time() - self.t_start)
-            ep_info.update(self.current_reset_info)
-            if self.results_writer:
-                self.results_writer.write_row(ep_info)
-            info["episode"] = ep_info
-        self.total_steps += 1
-        return observation, reward, done, info
+        except:
+            return self.env.state, 0, false, {}
 
     def close(self) -> None:
         """
